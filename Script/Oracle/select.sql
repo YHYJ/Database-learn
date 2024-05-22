c45d8d5255c95cc3be71270da6c3fbd0a59f3975
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-05-22 14:00:49 +0800

    添加 ORA-00376 问题查询语句

--- Script/Oracle/select.sql
+++ Script/Oracle/select.sql
@@ -56,35 +56,45 @@ AND "TIMESTAMP" < "TO_DATE" (
 
 -- 查询指定TIMESTAMP（可替换为其他字段）内所有"STATION"字段值以'S1'或'S2'开头的数据
 SELECT
-	*
+    *
 FROM
     TableName
 WHERE
-	(
-		STATION LIKE 'S1%'
-		OR STATION LIKE 'S2%'
-	)
+    (
+        STATION LIKE 'S1%'
+        OR STATION LIKE 'S2%'
+    )
 AND "TIMESTAMP" > "TO_DATE" (
-	'2023-02-06 00:00:00',
-	'yyyy-mm-dd hh24:mi:ss'
+    '2023-02-06 00:00:00',
+    'yyyy-mm-dd hh24:mi:ss'
 )
 AND "TIMESTAMP" < "TO_DATE" (
-	'2023-02-09 00:00:00',
-	'yyyy-mm-dd hh24:mi:ss'
+    '2023-02-09 00:00:00',
+    'yyyy-mm-dd hh24:mi:ss'
 )
 
 -- 查询指定TIMESTAMP（可替换为其他字段）内所有指定"STATION"值的数据，并根据TIMESTAMP倒序排列
 SELECT
-	*
+    *
 FROM
     TableName
 WHERE
-	(
-		STATION = 'S1'
-	)
+    (
+        STATION = 'S1'
+    )
 AND "TIMESTAMP" > "TO_DATE" (
-	'2023-02-06 00:00:00',
-	'yyyy-mm-dd hh24:mi:ss'
+    '2023-02-06 00:00:00',
+    'yyyy-mm-dd hh24:mi:ss'
 )
 ORDER BY
-	"TIMESTAMP" DESC
+    "TIMESTAMP" DESC
+
+-- 报错：ORA-00376: 此时无法读取文件 583
+-- 确认 ID 为 583 的文件的状态（正常是 "ONLINE"）
+SELECT
+    file_name,
+    status
+FROM
+    dba_data_files
+WHERE
+    file_id = 583;
